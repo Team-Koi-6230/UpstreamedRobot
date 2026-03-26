56dc21167bb0bd8735c8d36d40dedfbad0e7ed9e
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2026-03-26 12:14:37 +0200

    Refactor Robot class initialization and imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,16 +1,14 @@
 package frc.robot;
 
-import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
-import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-
-import team6230.koiupstream.superstates.Superstate;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import team6230.koiupstream.superstates.Superstate;
+import team6230.koiupstream.tunable.TunableManager;
 
 public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
@@ -18,24 +16,22 @@ public class Robot extends LoggedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
-
     Logger.recordMetadata("ProjectName", "Team Koi Robot code");
 
     if (isReal()) {
       Logger.addDataReceiver(new WPILOGWriter());
       Logger.addDataReceiver(new NT4Publisher());
     } else {
-      setUseTiming(false);
-      String logPath = LogFileUtil.findReplayLog();
-      Logger.setReplaySource(new WPILOGReader(logPath));
-      Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+      Logger.addDataReceiver(new NT4Publisher());
+      // Logger.addDataReceiver(new WPILOGWriter(""));
     }
 
     Logger.start();
 
-    Superstate.getInstance()
-          .setSuperstateSet(RobotState.IDLE);
+    m_robotContainer = new RobotContainer();
+
+    Superstate.getInstance().setSuperstateSet(RobotState.IDLE);
+    TunableManager.tuningModeEnabled = Constants.tuningMode;
   }
 
   @Override
